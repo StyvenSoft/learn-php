be3b8c9fd27ed2566a357d4d9149c9e034f2ad00
Steveen <steveen.echeverri@gmail.com>
2021-10-16 23:14:59 -0500

    Inheritance php class

--- Objects/class/Person.php
+++ Objects/class/Person.php
@@ -1,13 +1,14 @@
 <?php
 
-class Person{
+class Person {
     // Propiedades
-    public $name, $lastname, $age;
+    public $name, $age;
+    public $lastname1, $lastname2;
 
     // Metodo constructor
     public function __construct($name, $lastname, $age) {
         $this->name = strtolower($name);
-        $this->lastname = strtolower($lastname);
+        // $this->lastname = strtolower($lastname);
         $this->age = $age;
     }
 
@@ -19,4 +20,25 @@ class Person{
     public function getName() {
         return ucwords($this->name);
     }
+
+    public function setLastNames($lastname1, $lastname2) {
+        $this->lastname1 = $lastname1;
+        $this->lastname2 = $lastname2;
+    }
+
+    public function getLastNames() {
+        return $this->lastname1 ." ". $this->lastname2; 
+    }
 }
+
+// Herencia de clase
+class Colombian extends Person {
+    public $city, $region;
+
+    // Extender el alcance de un metodo
+    public function setLastNames($lastname1, $lastname2) {
+        parent::setLastNames($lastname1, $lastname2);
+        echo "Apellidos asegnados";
+    }
+}
+
--- Objects/index.php
+++ Objects/index.php
@@ -15,7 +15,11 @@ $personTwo = new Person("Lorena", "Rosada", 21);
 $personTwo->lastname = "Rosada";
 $personTwo->age = 21; */
 
+$personThree = new Colombian("Luis", "Diaz", 24);
+
 echo "Nombre completo: " . $personOne->getName() .' '. $personOne->lastname . "\n";
 echo "Nombre completo: " . $personTwo->name .' '. $personTwo->lastname . "\n";
 
+
 var_dump($personOne);
+var_dump($personThree);
